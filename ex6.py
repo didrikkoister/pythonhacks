42c8b2d5edc26a1251bff81c89dd343b3a8ce3f1
didrikkoister <didrik@eightcut.com>
2016-03-20 13:54:58 -0700

    recursive add

--- ex6.py
+++ ex6.py
@@ -9,4 +9,15 @@ def translate(str):
             newstr = newstr + i
     return newstr
 
+def translater(str):
+    conso = ["q","w","r","t","p","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m"]
+    Uconso = ["Q","W","R","T","P","S","D","F","G","H","J","K","L","Z","X","C","V","B","N","M"]
+    print str
+    if len(str) < 1:
+        return ""
+    elif str[0] in conso or str[0] in Uconso:
+        return  str[0] + "o" + str[0] + translater(str[1:])
+    else:
+        return str[0] + translater(str[1:])
+
 

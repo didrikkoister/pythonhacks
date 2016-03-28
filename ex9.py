8b90c0e3dd7d4ab8246219ddbe707057f1ea65fc
didrikkoister <didrik@eightcut.com>
2016-03-27 20:49:31 -0600

    ex9 final

--- ex9.py
+++ ex9.py
@@ -1,8 +1,38 @@
 from ex8 import reverse
 
 
+
+#removes unwanted signs and spaces 
+def cleanstring(str):
+    cleanout = " ,;:$%"
+    for i in cleanout:
+        str = str.replace(i,"")
+        #print str
+    return str
+
+
+def mycleanstring(str):
+    newstr = ""
+    for i in str:
+        if i == " ":
+            newstr = newstr
+        elif i == ",":
+            newstr = newstr
+        elif i == ";":
+            newstr = newstr
+        elif i == ":":
+            newstr = newstr
+        elif i == "$":
+            newstr = newstr
+        elif i == "%":
+            newstr = newstr
+        else:
+            newstr = newstr +i
+       # print newstr
+    return newstr
+
 def is_palindrome2(str):
-    str1 = str.replace(" ","").lower()
+    str1 = mycleanstring(str).lower()
     print str1
     newstring = ""
     for i in str1:
@@ -11,11 +41,7 @@ def is_palindrome2(str):
         return True
     else:
         return False
-def cleanstring(str):
-    cleanout = " ,;:$%"
-    for i in cleanout:
-        str = str.replace(i,"")
-    return str
+
 
 
 def is_palindrome(str):

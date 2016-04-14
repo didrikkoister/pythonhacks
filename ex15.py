2af6d52c76ac2852c801b9d60b160e8599f99b39
didrikkoister <didrik@eightcut.com>
2016-04-13 21:02:04 -0600

    ex15

--- ex15.py
+++ ex15.py
@@ -1,10 +1,10 @@
 def max_in_list(values):
-    if not (type(values) is list):
-        #print "Not a list"
+    if not (type(values) is list) or len(values) < 1:
+        print "Not a list"
         return False
     max = values[0]
     for i in values[1:]:
-        print "max is" + max
+        print "max is" +str(max)
         if i > max:
             max = i
     return max
